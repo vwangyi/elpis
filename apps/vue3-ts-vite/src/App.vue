dc5416a7563b661abf77a330a02912e6922814d5
wangyi <vwangyi@foxmail.com>
2026-07-21 22:43:18 +0800

    feat(root): ts

--- apps/vue3-ts-vite/src/App.vue
+++ apps/vue3-ts-vite/src/App.vue
@@ -1,4 +1,12 @@
-<script lang="ts"></script>
+<script lang="ts">
+/* eslint-disable */
+class User {}
+class User2 {}
+
+type twoParams = new (arg1: any, arg2: any) => User;
+
+type Inst = InstanceType<twoParams>; // 得到twoParams的返回值类型
+</script>
 
 <template>
   <div>hello</div>
