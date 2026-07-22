e9adb2068addf628d7f8768580ae94d4296fe6b0
wangyi <vwangyi@foxmail.com>
2026-07-22 21:13:25 +0800

    feat(demo): demo

--- apps/new-world/src/App.tsx
+++ apps/new-world/src/App.tsx
@@ -1,5 +1,5 @@
-import { RouterView } from "vue-router"
-function setup () {
-  return () => <RouterView /> 
+import { RouterView } from 'vue-router'
+function setup() {
+  return () => <RouterView />
 }
 export default { setup }
