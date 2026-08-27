9ca8da5462c28bdd375c1f9b717f31c82a792f7b
wangyi <vwangyi@foxmail.com>
2026-08-28 00:28:11 +0800

    feat(root): nest

--- apps/new-world/src/views/DemoView/views/CodingView/CodingView.vue
+++ apps/new-world/src/views/DemoView/views/CodingView/CodingView.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup>
-console.log(1);
+const test = `
+  let a = 1;
+  function a() {
+    console.log(a);
+  }
+  console.log(a);
+`;
 </script>
 
 <template>
@@ -7,4 +13,8 @@ console.log(1);
   <div>2. 手写 instanceof原理</div>
   <div>3. 手写 new操作符原理</div>
   <div>4. 手写 call/bind/apply</div>
+  <pre>
+  <code>
+    {{ test }}
+  </code></pre>
 </template>
